9c3ef40fc31733c29af75bf7eaf330663a23d4d1
Arsh Kohli <kohliarsh9@gmail.com>
2021-09-30 22:33:24 +0530

    add code for max subarray sum problem

---
+++ Array/C++/max_subarray_sum.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#include <climits>
+using namespace std;
+
+
+class Solution{
+    public:
+    long long maxSubarraySum(int arr[], int n){
+        long long max_so_far = INT_MIN;
+        long long max_rn = 0;
+        for (int i = 0; i < n; i++){
+            max_rn = max_rn + arr[i];
+            max_so_far = max(max_so_far, max_rn);
+            if (max_rn < 0) max_rn = 0;
+        }
+        return max_so_far;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
