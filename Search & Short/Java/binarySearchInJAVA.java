69f13c4687078c4e379a182880255b654d1a0e8c
Vishal Chauhan <44545745+vish-han@users.noreply.github.com>
2021-09-30 12:46:09 +0530

    Create binarySearchInJAVA.java

---
+++ Search & Short/Java/binarySearchInJAVA.java
@@ -0,0 +1,26 @@
+//this algorithm is known as Binary Search Algorithm;
+//the complexity of this algorithm is O(logn)
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 12, 34, 213, 232};
+        System.out.println(cieling(arr, 234));
+        System.out.println(floor(arr, 0));
+    }
+
+    static int bsearch(int[] arr, int target) {
+        if(arr[arr.length-1]<target){return -1;}
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (target < arr[mid]) {
+                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else return mid;
+        }
+
+        return -1;
+    }
