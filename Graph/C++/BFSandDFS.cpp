8e4d7100db9f447e40780c462e66ad91c443975b
MuditWadhwa <muditwadhwa1@gmail.com>
2021-10-01 03:15:31 +0530

    Completed explanation of DFS and BFS.Added input and decision by the user

--- Graph/C++/BFSandDFS.cpp
+++ Graph/C++/BFSandDFS.cpp
@@ -28,36 +28,37 @@ class Graph{
         }
     }
 void bfs(T u)
-{
-   queue<T> q;
+    {
+        queue<T> q;
            q.push(u);
         map<T,bool> visited;
         
         while(!q.empty())
-        {  T front_element=q.front();
-            q.pop();
-            if(!visited[front_element])  
+        {  T front_element=q.front();//record/store the first element
+            q.pop(); //pop the front element
+            if(!visited[front_element])//visited[front_element]==0 or false  
             {
                 cout<<front_element<<"->";
-                visited[front_element]=true; 
+                visited[front_element]=true; //if visited map does not have the front_element,make it true
             }
-            for(auto x:adjList[front_element])
+            for(auto x:adjList[front_element])//traverse over the pairs in the list corresponding to front_element
             {
-                if(!visited[front_element])   
+                if(!visited[front_element])   //if front_element is not present in visited(as it is false)
                 {
-                    q.push(x.first);           
+                    q.push(x.first);          //add the element(u) not traversed yet in the graph inside the queue 
                 }
             }
         }
-}
+    }
 void dfs_utility(T src)
     {
         map<T,bool> visited;               //use a map to decide whether a node has been visited or not
         dfs_helper(src,visited);          //the src node is the first node that is input into the graph.
     }
     void dfs_helper(T src,map<T,bool> &visited)
-    {
+    {    cout<<src<<' ';
         visited[src]=true;              //As the first node(src node) is where the dfs starts,consider it is already visited(true) 
+        
         for(auto x:adjList)            //traverse over the pairs present in the adjacency list
         {
             if(visited[x.first]==false) //if the node (u) has not been visited
@@ -70,14 +71,37 @@ void dfs_utility(T src)
 
 int main()
 {   Graph<int> g;
-     int n;
-     cin>>n;
+    // Enter n to fill graph.
+    // Then,If you want to simply print the graph,
+    //  enter 0.If you want to use BFS,enter 1.If you want to use DFS,enter 2.;
+   
+    int n,decision;
+     cin>>n>>decision;
     for(int i=0;i<n;++i)
     {
         g.addEdge(i,i+1,0);
-        g.print_adj();
-        //g.bfs(i);
+        //g.print_adj();
+
     }
-   
+    
+    if(decision==0)
+    { 
+        g.print_adj();
+    }
+    else if(decision==1)
+    { 
+      for(int i=0;i<n;++i)
+      {
+        g.bfs(i);
+       }   
+    }
+    else if(decision==2)
+    {
+       cout<<"Enter source node to start DFS"<<"\n";
+    int src;
+    cin>>src;
+   g.dfs_utility(src);    
+    }
+    
     return 0;
 }
