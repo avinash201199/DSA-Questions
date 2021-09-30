ace3ad7f4ff87f0e96888792caf6e8405e73c057
Kushagrasaxena-13 <71897053+Kushagrasaxena-13@users.noreply.github.com>
2021-09-30 17:02:18 +0530

    Created Prime Sieve

---
+++ Dynamic Programming/C++/Prime_sieve.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void SieveOfEratosthenes(int n)
+{
+    // Create a boolean array
+
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+
+    for (int p = 2; p * p <= n; p++)
+    {
+        // If prime[p] is not changed,
+        // then it is a prime
+        if (prime[p] == true)
+        {
+
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+
+    // Print all prime numbers
+    for (int p = 2; p <= n; p++)
+        if (prime[p])
+            cout << p << " ";
+}
+
+// Driver Code
+int main()
+{
+    int n = 30;
+    cout << "Follwoing are nos smaller than or equal to  "<< n << endl;
+    SieveOfEratosthenes(n);
+    return 0;
+}
