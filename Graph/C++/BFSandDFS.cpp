c739369df32cf406436d8ed13e5437d3127b2846
MuditWadhwa <muditwadhwa1@gmail.com>
2021-10-01 02:28:09 +0530

    Added BFS function

--- Graph/C++/BFSandDFS.cpp
+++ Graph/C++/BFSandDFS.cpp
@@ -27,7 +27,31 @@ class Graph{
         cout<<"\n";
         }
     }
+void bfs(T u)
+{
+   queue<T> q;
+           q.push(u);
+        map<T,bool> visited;
+        
+        while(!q.empty())
+        {  T front_element=q.front();//record/store the first element
+            q.pop(); //pop the front element
+            if(!visited[front_element])//visited[front_element]==0 or false  
+            {
+                cout<<front_element<<"->";
+                visited[front_element]=true; //if visited map does not have the front_element,make it true
+            }
+            for(auto x:adjList[front_element])//traverse over the pairs in the list corresponding to front_element
+            {
+                if(!visited[front_element])   //if front_element is not present in visited(as it is false)
+                {
+                    q.push(x.first);          //add the element(u) not traversed yet in the graph inside the queue 
+                }
+            }
+        }
+}
 };
+
 int main()
 {   Graph<int> g;
      int n;
@@ -36,6 +60,8 @@ int main()
     {
         g.addEdge(i,i+1,0);
         g.print_adj();
+        g.bfs(i);
     }
+   
     return 0;
 }
