03ec6d0fb98f143c37b93a3bde0bb1671099a207
Anushka Dubey <71165722+anushkadubey@users.noreply.github.com>
2021-10-01 12:54:54 +0530

    Update Remove_All_Adjacent_Duplicates_in_String.cpp

--- Stacks & Queues/C++/Remove_All_Adjacent_Duplicates_in_String.cpp
+++ Stacks & Queues/C++/Remove_All_Adjacent_Duplicates_in_String.cpp
@@ -16,25 +16,24 @@ int main() {
 }
 
    string removeDuplicates(string s, int k) {
-        stack <pair<char,int>> st;
-        string ans;
-        int count=0;
-        for(int i=0; i<s.length(); i++){
-            if(!st.empty() and st.top().first==s[i]){
-                if(st.top().second<k-1)
-                    st.push({s[i], st.top().second+1});
-                else
-                    while(!st.empty() and st.top().first==s[i])
-                    st.pop();
+        stack <pair<char,int>> st;   	       				//declaring a stack pair to store characters of string and its index number
+        string ans;           	      	     				// declaring string to store the output string
+        for(int i=0; i<s.length(); i++){      				//traversing through each character of input string
+            if(!st.empty() and st.top().first==s[i]){			//comparing first element of top of stack pair to the string character
+                if(st.top().second<k-1)					//if the second element of stack pair is less than k-1, 
+                    st.push({s[i], st.top().second+1});			//push it in the stack and increase second element of stack pair by 1
+                else							//else
+                    while(!st.empty() and st.top().first==s[i])		//stack.pop() until the stack.top() is equal to first element of stack pair 
+                    st.pop();						//or until the list gets empty
             }
-           else{
-               st.push({s[i], 1});
+           else{							//else if the stack is empty or stack.top().first is not equal to string character
+               st.push({s[i], 1});					//push it to the stack and give second element of stack as 1
         }
         }
-       while(st.size()>0){
-               ans.push_back(st.top().first);
+       while(st.size()>0){						//Now that we have traversed the whole string, characters left in the stack comprise
+               ans.push_back(st.top().first);				//of output string, hence pushing them into thr ans string
                st.pop();
        }
-        reverse(ans.begin(), ans.end());
-        return ans;
+        reverse(ans.begin(), ans.end());				//reversing the ans string
+        return ans;							//returning ans
     }
