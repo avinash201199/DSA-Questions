bc35f0311b62dff60b0ddcc0aaea21c112bc415d
Suprodip Kundu <suprodipkundu718@gmail.com>
2021-10-01 13:49:39 +0530

    Add files via upload

---
+++ Array/C++/Maximum_Distance.cpp.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+  int maxDistance(int arr[], int n)
+    {
+        unordered_map<int, int> mp;
+        
+        int res = 0;
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(mp.find(arr[i]) != mp.end())
+            {
+                res = max(res, i - mp[arr[i]]);
+            }
+            else
+            {
+                mp[arr[i]] = i;
+            }
+        }
+        
+        return res;
+    }  
+
+    int main()
+
+    {
+	    int arr[]={1,1,2,2,2,1};
+	    int n = sizeof(arr)/sizeof(int);
+	    int sum=maxDistance(arr,n);
+	    cout<<sum;
+	    return 0;
+    }
