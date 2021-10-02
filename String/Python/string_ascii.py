64f55229a3664047ca627e13e4dd5fdf1679cd32
Mayank Goyal <65842833+mayankgoyal-13@users.noreply.github.com>
2021-10-02 18:34:17 +0530

    Create string_ascii.py

---
+++ String/Python/string_ascii.py
@@ -0,0 +1,9 @@
+"""
+Name : Quadratic equation solver
+Author : [Mayank Goyal) [https://github.com/mayankgoyal-13]
+Question : Input a string from the user and give the ascii value of all the characters
+"""
+
+str = input("Enter any String :")
+for i in str:
+    print("ASCII value of ", i, "is", ord(i))
