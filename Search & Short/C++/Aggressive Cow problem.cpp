23a960d8a85aba31443fc7fc48eea6abc75eb8d8
MuditWadhwa <muditwadhwa1@gmail.com>
2021-10-02 18:18:35 +0530

    Explanation for checkcow function is completed using comments

--- Search & Short/C++/Aggressive Cow problem.cpp
+++ Search & Short/C++/Aggressive Cow problem.cpp
@@ -7,19 +7,20 @@ bool checkcow(vector<ll> &stalls,ll numberofcows,ll numberofstalls, ll min_dista
 {
 ll recent_cow=stalls[0];//Assume that first value is the cow that has been chosen
 ll counter=1;           //counter will be used to break the loop when all cows have been counted
-   for(ll i=1;i<numberofstalls;++i)
+
+   for(ll i=1;i<numberofstalls;++i)//Start loop from 1 as we have already chosen the cow at the 0th index 
    {
-       if(stalls[i]-recent_cow>=min_distance)
+       if(stalls[i]-recent_cow>=min_distance)//If a distance larger than or equal to mid value is found
        {
-           recent_cow=stalls[i];
+           recent_cow=stalls[i];//Update mininum value  
             ++counter;
-            if(counter==numberofcows)
+            if(counter==numberofcows)//check if all cows have been counted
             {
                 return true;
             }
        }
    }
-   return false;
+   return false;//If all cows have been placed correctly,return true else false
 }
 int main()
 {   int t; 
